4e4a952ed28620883904af1337a48aacbaaf5fc8
kisha02 <w.snijders1989@gmail.com>
2020-11-05 22:13:00 +0100

    cleanup

--- main.js
+++ main.js
@@ -2,31 +2,11 @@ const { app, BrowserWindow } = require('electron')
 if (require('electron-squirrel-startup')) return app.quit();
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 940,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-	  enableRemoteModule: true
-    }, 
-	frame: false,
-  })  
-  //win.openDevTools();
-  //win.removeMenu()
-  //win.setResizable(false)
-  win.loadFile('index.html')
+	const win = new BrowserWindow({width: 940,height: 600,webPreferences: {nodeIntegration: true,enableRemoteModule: true}, frame: false,})  
+	win.loadFile('index.html')
+	//win.openDevTools();
 }
 
 app.whenReady().then(createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+app.on('window-all-closed', () => {if (process.platform !== 'darwin') {app.quit()}})
+app.on('activate', () => {if (BrowserWindow.getAllWindows().length === 0) {createWindow()}})
