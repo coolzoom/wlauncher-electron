d15e269277cf1565c8657c500df0f8f41c189990
kisha02 <w.snijders1989@gmail.com>
2020-11-13 12:14:30 +0100

    add rounded corners

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron')
 if (require('electron-squirrel-startup')) return app.quit();
 
 function createWindow () {
-	const win = new BrowserWindow({width: 940, height: 600, minWidth: 940, minHeight: 600, maxHeight: 600, maxWidth: 940, webPreferences: {nodeIntegration: true,enableRemoteModule: true}, frame: false,})  
+	const win = new BrowserWindow({width: 940, height: 600, minWidth: 940, minHeight: 600, maxHeight: 600, maxWidth: 940, webPreferences: {nodeIntegration: true,enableRemoteModule: true}, transparent: true,  frame: false,})  
 	win.loadFile('index.html')
 	//win.openDevTools();
 }
